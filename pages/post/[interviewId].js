0442f143c917a8db74d44721ac7875e8fc197404
Ioana-Cristina Socaciu <dramaticgarlic@gmail.com>
2023-08-08 16:50:18 +0200

    thumbs icon added and counter increases likes

--- pages/post/[interviewId].js
+++ pages/post/[interviewId].js
@@ -3,14 +3,19 @@ import { useRouter } from "next/router";
 import { styled } from "styled-components";
 import { interviews } from "@/public/interviews";
 import Markdown from "markdown-to-jsx";
+import { useState } from "react";
+import ThumbUps from "@/public/thumbs-up-regular.svg";
 
 export default function InterviewArticle() {
   const router = useRouter();
   const interviewId = router.query.interviewId;
+  const [likes, setLikes] = useState(0);
+
   const article = interviews.find((interview) => interview.id == interviewId);
   if (!article) {
     return null;
   }
+
   return (
     <article>
       <TitleContainer>{article?.title}</TitleContainer>
@@ -19,6 +24,10 @@ export default function InterviewArticle() {
       </ImageContainer>
       <TextContainer>
         <Markdown>{article?.text}</Markdown>
+        <ThumbUpContainer>
+          <ThumbUpIcon onClick={() => setLikes(likes + 1)} />
+          <span>{likes}</span>
+        </ThumbUpContainer>
       </TextContainer>
     </article>
   );
@@ -50,4 +59,15 @@ const TitleContainer = styled.h2`
   color: #95091b;
   line-height: 1.4;
   margin-top: 0;
+`;
+const ThumbUpContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+`;
+const ThumbUpIcon = styled(ThumbUps)`
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+  /* fill: #95091b; */
 `;
