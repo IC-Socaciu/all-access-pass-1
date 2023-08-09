9b4b6d84e069d0d52f68643373351fab126812f7
Ioana-Cristina Socaciu <dramaticgarlic@gmail.com>
2023-08-09 11:49:29 +0200

    comment section added and functionality - messages are now stored locally

--- pages/post/[interviewId].js
+++ pages/post/[interviewId].js
@@ -10,13 +10,24 @@ export default function InterviewArticle() {
   const router = useRouter();
   const interviewId = router.query.interviewId;
   const [likes, setLikes] = useState(0);
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const [comments, setComments] = useState([]);
+  const [messageLength, setMessageLength] = useState(0);
 
   useEffect(() => {
     const savedLikes = localStorage.getItem(`likes${interviewId}`);
     if (savedLikes !== null) {
       setLikes(parseInt(savedLikes));
     }
+    const savedComments = localStorage.getItem(`comments${interviewId}`);
+    if (savedComments !== null) {
+      setComments(JSON.parse(savedComments));
+    }
   }, [interviewId]);
+  useEffect(() => {
+    localStorage.setItem(`comments${interviewId}`, JSON.stringify(comments));
+  }, [comments, interviewId]);
 
   const article = interviews.find((interview) => interview.id == interviewId);
   if (!article) {
@@ -27,6 +38,32 @@ export default function InterviewArticle() {
     const newLikes = likes + 1;
     setLikes(newLikes);
     localStorage.setItem(`likes${interviewId}`, newLikes.toString());
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+    setMessageLength(event.target.value.length);
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    if (name === "" || message === "") {
+      alert("Please enter a name and message");
+      return;
+    }
+    const newComment = {
+      name: name,
+      message: message,
+    };
+    const updatedComments = [...comments, newComment];
+    setComments(updatedComments);
+    setName("");
+    setMessage("");
+    setMessageLength(0);
   };
 
   return (
@@ -42,6 +79,38 @@ export default function InterviewArticle() {
       <TextContainer>
         <Markdown>{article?.text}</Markdown>
       </TextContainer>
+      <CommentsContainer>
+        <form onSubmit={handleFormSubmit}>
+          <InputContainer>
+            <Label htmlFor="name">Name:</Label>
+            <TextInput
+              id="name"
+              type="text"
+              value={name}
+              onChange={handleNameChange}
+            />
+          </InputContainer>
+          <InputContainer>
+            <Label htmlFor="message">Message:</Label>
+            <TextArea
+              id="message"
+              value={message}
+              onChange={handleMessageChange}
+              maxlength="150"
+            />
+            <CharCount>{messageLength}/150</CharCount>
+          </InputContainer>
+          <ButtonContainer>
+            <SubmitButton type="submit">Submit</SubmitButton>
+          </ButtonContainer>
+        </form>
+        {comments.map((comment, index) => (
+          <Comment key={index}>
+            <CommentName>User {comment.name} wrote:</CommentName>
+            <CommentMessage>{comment.message}</CommentMessage>
+          </Comment>
+        ))}
+      </CommentsContainer>
     </article>
   );
 }
@@ -84,4 +153,64 @@ const ThumbUpIcon = styled(ThumbUps)`
   margin-right: 5px;
   fill: #95091b;
   margin-left: 20px;
+`;
+const CommentsContainer = styled.div`
+  margin-top: 20px;
+  margin-bottom: 200px;
+`;
+const InputContainer = styled.div`
+  margin-bottom: 10px;
+`;
+const Label = styled.label`
+  display: block;
+`;
+const TextInput = styled.input`
+  width: 100%;
+  padding: 5px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  outline: none;
+`;
+const TextArea = styled.textarea`
+  width: 100%;
+  padding: 5px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  outline: none;
+  resize: vertical;
+`;
+const ButtonContainer = styled.div`
+  text-align: right;
+  margin-bottom: 20px;
+`;
+const SubmitButton = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  border-radius: 5px;
+  background-color: #95091b;
+  margin-top: 10px;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  &:hover {
+    background-color: #731c28;
+  }
+`;
+const Comment = styled.div`
+  margin-bottom: 20px;
+  border: 1px solid #95091b;
+`;
+const CommentName = styled.div`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+const CommentMessage = styled.div`
+  white-space: pre-wrap;
+`;
+const CharCount = styled.div`
+  font-size: 12px;
+  margin-top: 5px;
 `;
