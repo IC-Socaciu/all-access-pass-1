341b0727c48b752643d985e1a00bb116938f4ccd
Ioana-Cristina Socaciu <dramaticgarlic@gmail.com>
2023-08-11 15:35:20 +0200

    page created

---
+++ pages/discover.js
@@ -0,0 +1,12 @@
+import Footer from "@/components/Footer";
+import SearchInput from "@/components/Search/SearchInput";
+
+export default function Discover() {
+  return (
+    <>
+      <h2>Discover!</h2>
+      <h3>TextTextText</h3>
+      <button>Click to discover!</button>
+    </>
+  );
+}
